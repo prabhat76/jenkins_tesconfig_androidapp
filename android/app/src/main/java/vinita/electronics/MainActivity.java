b41a5b90117acc2a1071363cbd8cb7436daee8c8
prabhat76 <prabhatprabhakar8@gmail.com>
2025-05-31 00:45:49 +0530

    first commit

---
+++ android/app/src/main/java/vinita/electronics/MainActivity.java
@@ -0,0 +1,5 @@
+package vinita.electronics;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vinita.electronics',
+  appName: 'vinita-electronics',
+  webDir: 'dist'
+};
+
+export default config;
